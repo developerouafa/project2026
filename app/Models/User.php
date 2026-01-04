335f517b3a5be17b3ab57196f5fcbd9ca9830481
Ouafa <92248542+developerouafa@users.noreply.github.com>
2026-01-04 20:53:46 +0100

    Package Roldes Permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,13 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Spatie\Translatable\HasTranslations;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes, HasApiTokens, HasTranslations;
+    use HasFactory, Notifiable, SoftDeletes, HasApiTokens, HasTranslations, HasRoles;
 
     /**
      * The attributes that are mass assignable.
