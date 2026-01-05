9fd9915f1459fe37974a5b35ba342dd0575b7629
Ouafa <92248542+developerouafa@users.noreply.github.com>
2026-01-05 12:47:57 +0100

    getLocale CSS

--- resources/views/Dashboard_UMC/layouts/head.blade.php
+++ resources/views/Dashboard_UMC/layouts/head.blade.php
@@ -1,5 +1,8 @@
 <!-- Title -->
-<title> Valex -  Premium dashboard ui bootstrap rwd admin html5 template </title>
+<title>@yield('title')</title>
+
+@yield('css')
+
 <!-- Favicon -->
 <link rel="icon" href="{{URL::asset('assets/img/brand/favicon.png')}}" type="image/x-icon"/>
 <!-- Icons css -->
@@ -8,12 +11,38 @@
 <link href="{{URL::asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
 <!--  Sidebar css -->
 <link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
-<!-- Sidemenu css -->
-<link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
-@yield('css')
-<!--- Style css -->
-<link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
-<!--- Dark-mode css -->
-<link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
-<!---Skinmodes css-->
-<link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+<!-- Maps css -->
+<link href="{{URL::asset('assets/plugins/jqvmap/jqvmap.min.css')}}" rel="stylesheet">
+
+<link href="{{URL::asset('assets/plugins/datatable/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/buttons.bootstrap4.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" />
+<link href="{{URL::asset('assets/plugins/datatable/css/jquery.dataTables.min.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/plugins/datatable/css/responsive.dataTables.min.css')}}" rel="stylesheet">
+
+@if(App::getLocale() =='ar')
+    <!--  Sidebar css -->
+    <link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+    <!-- Sidemenu css -->
+    <link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
+    <!--- Style css -->
+    <link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
+    <!--- Dark-mode css -->
+    <link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+    <!---Skinmodes css-->
+    <link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+
+@else
+    <!--  Right-sidemenu css -->
+    <link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
+    <!-- Sidemenu css -->
+    <link rel="stylesheet" href="{{URL::asset('assets/css/sidemenu.css')}}">
+    <!-- style css -->
+    <link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
+    <!--- Dark-mode css -->
+    <link href="{{URL::asset('assets/css/style-dark.css')}}" rel="stylesheet">
+    <!---Skinmodes css-->
+    <link href="{{URL::asset('assets/css/skin-modes.css')}}" rel="stylesheet" />
+
+@endif
+
