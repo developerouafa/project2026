87dda40baadf59e1d3311d4494deed2a97e02570
Ouafa <92248542+developerouafa@users.noreply.github.com>
2026-01-07 11:33:47 +0100

    tabel & model Client

---
+++ app/Models/Client.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Spatie\Translatable\HasTranslations;
+
+class Client extends Authenticatable implements MustVerifyEmail
+{
+    use HasFactory, Notifiable, SoftDeletes, HasApiTokens, HasTranslations, HasRoles;
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'email_verified_at',
+        'password',
+        'image',
+        'can_login',
+        'account_state',
+    ];
+
+    public array $translatable = ['name'];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+}
